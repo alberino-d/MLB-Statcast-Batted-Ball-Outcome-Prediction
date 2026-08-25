c900fa275031c3b8eb036dd8eeb7058d86dbd228
David Alberino Jr <alberino.d@northeastern.edu>
2026-08-25 12:29:08 -0400

    created batted balls dataset

--- project.py
+++ project.py
@@ -1,1 +1,17 @@
+from pybaseball.datahelpers.statcast_utils import add_spray_angle
+from pybaseball import statcast
 
+df = statcast(
+    start_dt="2025-03-27",
+    end_dt="2025-09-28"
+)
+
+batted_balls = df[~df["bb_type"].isna()]
+batted_balls = add_spray_angle(batted_balls)
+
+hit_outcomes = ["single", "double", "triple", "home_run"]
+batted_balls["hit"] = (
+    batted_balls["events"].isin(hit_outcomes)
+).astype(int)
+
+batted_balls.to_csv("batted_balls.csv", index=False)
