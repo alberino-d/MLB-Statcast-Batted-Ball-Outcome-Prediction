2f1c748754c90ae4f64021876a78efd38beef101
David Alberino Jr <alberino.d@northeastern.edu>
2026-08-31 15:06:26 -0400

    created basic feature distribution graphs

--- eda.py
+++ eda.py
@@ -9,18 +9,100 @@ import numpy as np
 
 
 # create DataFrame from batted ball data
-bbdf = pd.read_csv("batted_balls.csv")
+bbdf = pd.read_csv('batted_balls.csv')
 
 features = [
-    "launch_speed",
-    "launch_angle",
-    "spray_angle",
-    "bb_type"
+    'launch_speed',
+    'launch_angle',
+    'spray_angle',
+    'bb_type'
+]
+
+hits = [
+    'single',
+    'double',
+    'triple',
+    'home_run'
 ]
 
 
 # describe numerical features
-print(bbdf[features].describe(include=[np.number]).round(2))
+# print(bbdf[features].describe(include=[np.number]).round(2))
+
 
 # describe categorical features
-print(bbdf[features + ["events"]].describe(include=[object]))
+# print(bbdf[features + ['events']].describe(include=[object]))
+
+
+# Plot Feature Distributions
+
+# Numeric Features
+def plot_feature_dist(feature):
+    # fix column name
+    feature_name = feature.replace('_', ' ').title()
+
+    fig, axes = plt.subplots(figsize=(10, 3), nrows=1, ncols=2)
+
+    # create histogram
+    sns.histplot(
+        x=bbdf[feature],
+        hue=bbdf['is_hit'],
+        bins=30,
+        palette={0:'steelblue', 1:'indianred'},
+        ax=axes[0]
+    )
+
+    # create violin plot
+    sns.violinplot(
+        x=bbdf[feature],
+        hue=bbdf['is_hit'],
+        split=True,
+        palette={0:'steelblue', 1:'indianred'},
+        ax=axes[1]
+    )
+
+    # create legend
+    handles, _ = plt.gca().get_legend_handles_labels()
+    axes[0].legend(handles, ['Out', 'Hit'], title='Outcome')
+    axes[1].legend(handles, ['Out', 'Hit'], title='Outcome')
+
+    # create axis
+    axes[0].set_xlabel(feature_name)
+    axes[1].set_xlabel(feature_name)
+    fig.suptitle(f'{feature_name} Distribution by Hit Outcome')
+
+    plt.show()
+
+# Categorical Features
+fig, ax = plt.subplots(figsize=(5,3))
+
+sns.barplot(
+    bbdf.groupby('bb_type')['events'].apply(lambda x: x.isin(hits).mean()),
+    linewidth=1,
+    edgecolor='black',
+    color='lightblue'
+)
+
+for p in ax.patches:
+    h = p.get_height()
+    ax.text(p.get_x()+p.get_width()/2,
+            h+0.02,
+            f'{h*100:.1f}%',
+            ha='center'
+    )
+
+plt.xlabel('Batted Ball Type')
+ax.set_xticks(['fly_ball', 'ground_ball', 'line_drive', 'popup'])
+ax.set_xticklabels(['Fly Ball', 'Ground Ball', 'Line Drive', 'Popup'])
+
+plt.ylim(0, 0.7)
+plt.ylabel('Hit Percentage')
+plt.yticks([])
+
+fig.suptitle('Pct. of Batted Balls Resulting in Hits by Type')
+
+plt.show()
+
+# print(plot_feature_dist('launch_speed'))
+# print(plot_feature_dist('launch_angle'))
+# print(plot_feature_dist('spray_angle'))
--- load_data.py
+++ load_data.py
@@ -7,25 +7,25 @@ from pybaseball import statcast
 
 # get statcast data
 df = statcast(
-    start_dt="2025-03-27",
-    end_dt="2025-09-28"
+    start_dt='2025-03-27',
+    end_dt='2025-09-28'
 )
 
 
 # create batted balls dataset
-# batted_balls = df[df["bb_type"].notna()]
-# batted_balls = add_spray_angle(batted_balls)
+batted_balls = df[df['bb_type'].notna()]
+batted_balls = add_spray_angle(batted_balls)
 
-hit_outcomes = ["single", "double", "triple", "home_run"]
-# batted_balls["is_hit"] = (
-#     batted_balls["events"].isin(hit_outcomes)
-# ).astype(int)
+hit_outcomes = ['single', 'double', 'triple', 'home_run']
+batted_balls['is_hit'] = (
+    batted_balls['events'].isin(hit_outcomes)
+).astype(int)
 
-# batted_balls.to_csv("batted_balls.csv", index=False)
+batted_balls.to_csv('batted_balls.csv', index=False)
 
 
-# created hits dataset
-hits = df[df["events"].isin(hit_outcomes)].copy()
+# create hits dataset
+hits = df[df['events'].isin(hit_outcomes)].copy()
 hits = add_spray_angle(hits)
 
-hits.to_csv("hits.csv", index=False)
+hits.to_csv('hits.csv', index=False)
