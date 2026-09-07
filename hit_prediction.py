54f9a434f9f3073c059b8ba130c40ce1c7812000
David Alberino Jr <alberino.d@northeastern.edu>
2026-09-07 00:38:36 -0400

    created shap graphs for xgboost models

--- hit_prediction.py
+++ hit_prediction.py
@@ -13,7 +13,8 @@ from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, log_loss, confusion_matrix, ConfusionMatrixDisplay
+import shap
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix, ConfusionMatrixDisplay
 
 
 # create DataFrame from batted ball data
@@ -419,67 +420,100 @@ def evaluate_model(model, X_te, y_te):
     return roc_auc, accuracy, precision, recall, f1, conf_mat
 
 # logistic regression without sprint
-lrwo_roc_auc, lrwo_accuracy, lrwo_precision, lrwo_recall, lrwo_f1, lrwo_conf_mat = evaluate_model(logistic_model_wo, X_te_wo, y_te)
-print('Logistic Regression w/o Sprint Performance:')
-print(f'Roc-Auc Score: {lrwo_roc_auc:.4f}')
-print(f'Accuracy Score: {lrwo_accuracy:.4f}')
-print(f'Precision Score: {lrwo_precision:.4f}')
-print(f'Recall Score: {lrwo_recall:.4f}')
-print(f'f1 Score: {lrwo_f1:.4f}')
-ConfusionMatrixDisplay(lrwo_conf_mat, display_labels=['Out', 'Hit']).plot(cmap='Blues')
-plt.show()
+# lrwo_roc_auc, lrwo_accuracy, lrwo_precision, lrwo_recall, lrwo_f1, lrwo_conf_mat = evaluate_model(logistic_model_wo, X_te_wo, y_te)
+# print('Logistic Regression w/o Sprint Performance:')
+# print(f'Roc-Auc Score: {lrwo_roc_auc:.4f}')
+# print(f'Accuracy Score: {lrwo_accuracy:.4f}')
+# print(f'Precision Score: {lrwo_precision:.4f}')
+# print(f'Recall Score: {lrwo_recall:.4f}')
+# print(f'f1 Score: {lrwo_f1:.4f}')
+# ConfusionMatrixDisplay(lrwo_conf_mat, display_labels=['Out', 'Hit']).plot(cmap='Blues')
+# plt.show()
 
 # logistic regression with sprint
-lrwith_roc_auc, lrwith_accuracy, lrwith_precision, lrwith_recall, lrwith_f1, lrwith_conf_mat = evaluate_model(logistic_model_with, X_te_with, y_te)
-print('Logistic Regression w/ Sprint Performance:')
-print(f'Roc-Auc Score: {lrwith_roc_auc:.4f}')
-print(f'Accuracy Score: {lrwith_accuracy:.4f}')
-print(f'Precision Score: {lrwith_precision:.4f}')
-print(f'Recall Score: {lrwith_recall:.4f}')
-print(f'f1 Score: {lrwith_f1:.4f}')
-ConfusionMatrixDisplay(lrwith_conf_mat, display_labels=['Out', 'Hit']).plot(cmap='Blues')
-plt.show()
+# lrwith_roc_auc, lrwith_accuracy, lrwith_precision, lrwith_recall, lrwith_f1, lrwith_conf_mat = evaluate_model(logistic_model_with, X_te_with, y_te)
+# print('Logistic Regression w/ Sprint Performance:')
+# print(f'Roc-Auc Score: {lrwith_roc_auc:.4f}')
+# print(f'Accuracy Score: {lrwith_accuracy:.4f}')
+# print(f'Precision Score: {lrwith_precision:.4f}')
+# print(f'Recall Score: {lrwith_recall:.4f}')
+# print(f'f1 Score: {lrwith_f1:.4f}')
+# ConfusionMatrixDisplay(lrwith_conf_mat, display_labels=['Out', 'Hit']).plot(cmap='Blues')
+# plt.show()
 
 # random forest without sprint
-rfwo_roc_auc, rfwo_accuracy, rfwo_precision, rfwo_recall, rfwo_f1, rfwo_conf_mat = evaluate_model(random_forest_model_wo, X_te_wo, y_te)
-print('Random Forest w/o Sprint Performance:')
-print(f'Roc-Auc Score: {rfwo_roc_auc:.4f}')
-print(f'Accuracy Score: {rfwo_accuracy:.4f}')
-print(f'Precision Score: {rfwo_precision:.4f}')
-print(f'Recall Score: {rfwo_recall:.4f}')
-print(f'f1 Score: {rfwo_f1:.4f}')
-ConfusionMatrixDisplay(rfwo_conf_mat, display_labels=['Out', 'Hit']).plot(cmap='Blues')
-plt.show()
+# rfwo_roc_auc, rfwo_accuracy, rfwo_precision, rfwo_recall, rfwo_f1, rfwo_conf_mat = evaluate_model(random_forest_model_wo, X_te_wo, y_te)
+# print('Random Forest w/o Sprint Performance:')
+# print(f'Roc-Auc Score: {rfwo_roc_auc:.4f}')
+# print(f'Accuracy Score: {rfwo_accuracy:.4f}')
+# print(f'Precision Score: {rfwo_precision:.4f}')
+# print(f'Recall Score: {rfwo_recall:.4f}')
+# print(f'f1 Score: {rfwo_f1:.4f}')
+# ConfusionMatrixDisplay(rfwo_conf_mat, display_labels=['Out', 'Hit']).plot(cmap='Blues')
+# plt.show()
 
 # random forest with sprint
-rfwith_roc_auc, rfwith_accuracy, rfwith_precision, rfwith_recall, rfwith_f1, rfwith_conf_mat = evaluate_model(random_forest_model_with, X_te_with, y_te)
-print('Random Forest w/ Sprint Performance:')
-print(f'Roc-Auc Score: {rfwith_roc_auc:.4f}')
-print(f'Accuracy Score: {rfwith_accuracy:.4f}')
-print(f'Precision Score: {rfwith_precision:.4f}')
-print(f'Recall Score: {rfwith_recall:.4f}')
-print(f'f1 Score: {rfwith_f1:.4f}')
-ConfusionMatrixDisplay(rfwith_conf_mat, display_labels=['Out', 'Hit']).plot(cmap='Blues')
-plt.show()
+# rfwith_roc_auc, rfwith_accuracy, rfwith_precision, rfwith_recall, rfwith_f1, rfwith_conf_mat = evaluate_model(random_forest_model_with, X_te_with, y_te)
+# print('Random Forest w/ Sprint Performance:')
+# print(f'Roc-Auc Score: {rfwith_roc_auc:.4f}')
+# print(f'Accuracy Score: {rfwith_accuracy:.4f}')
+# print(f'Precision Score: {rfwith_precision:.4f}')
+# print(f'Recall Score: {rfwith_recall:.4f}')
+# print(f'f1 Score: {rfwith_f1:.4f}')
+# ConfusionMatrixDisplay(rfwith_conf_mat, display_labels=['Out', 'Hit']).plot(cmap='Blues')
+# plt.show()
 
 # XGBoost without sprint
-xgbwo_roc_auc, xgbwo_accuracy, xgbwo_precision, xgbwo_recall, xgbwo_f1, xgbwo_conf_mat = evaluate_model(xgboost_model_wo, X_te_wo, y_te)
-print('XGBoost w/o Sprint Performance:')
-print(f'Roc-Auc Score: {xgbwo_roc_auc:.4f}')
-print(f'Accuracy Score: {xgbwo_accuracy:.4f}')
-print(f'Precision Score: {xgbwo_precision:.4f}')
-print(f'Recall Score: {xgbwo_recall:.4f}')
-print(f'f1 Score: {xgbwo_f1:.4f}')
-ConfusionMatrixDisplay(xgbwo_conf_mat, display_labels=['Out', 'Hit']).plot(cmap='Blues')
-plt.show()
+# xgbwo_roc_auc, xgbwo_accuracy, xgbwo_precision, xgbwo_recall, xgbwo_f1, xgbwo_conf_mat = evaluate_model(xgboost_model_wo, X_te_wo, y_te)
+# print('XGBoost w/o Sprint Performance:')
+# print(f'Roc-Auc Score: {xgbwo_roc_auc:.4f}')
+# print(f'Accuracy Score: {xgbwo_accuracy:.4f}')
+# print(f'Precision Score: {xgbwo_precision:.4f}')
+# print(f'Recall Score: {xgbwo_recall:.4f}')
+# print(f'f1 Score: {xgbwo_f1:.4f}')
+# ConfusionMatrixDisplay(xgbwo_conf_mat, display_labels=['Out', 'Hit']).plot(cmap='Blues')
+# plt.show()
 
 # XGBoost with sprint
-xgbwith_roc_auc, xgbwith_accuracy, xgbwith_precision, xgbwith_recall, xgbwith_f1, xgbwith_conf_mat = evaluate_model(xgboost_model_with, X_te_with, y_te)
-print('XGBoost w/ Sprint Performance:')
-print(f'Roc-Auc Score: {xgbwith_roc_auc:.4f}')
-print(f'Accuracy Score: {xgbwith_accuracy:.4f}')
-print(f'Precision Score: {xgbwith_precision:.4f}')
-print(f'Recall Score: {xgbwith_recall:.4f}')
-print(f'f1 Score: {xgbwith_f1:.4f}')
-ConfusionMatrixDisplay(xgbwith_conf_mat, display_labels=['Out', 'Hit']).plot(cmap='Blues')
-plt.show()
+# xgbwith_roc_auc, xgbwith_accuracy, xgbwith_precision, xgbwith_recall, xgbwith_f1, xgbwith_conf_mat = evaluate_model(xgboost_model_with, X_te_with, y_te)
+# print('XGBoost w/ Sprint Performance:')
+# print(f'Roc-Auc Score: {xgbwith_roc_auc:.4f}')
+# print(f'Accuracy Score: {xgbwith_accuracy:.4f}')
+# print(f'Precision Score: {xgbwith_precision:.4f}')
+# print(f'Recall Score: {xgbwith_recall:.4f}')
+# print(f'f1 Score: {xgbwith_f1:.4f}')
+# ConfusionMatrixDisplay(xgbwith_conf_mat, display_labels=['Out', 'Hit']).plot(cmap='Blues')
+# plt.show()
+
+
+
+
+# evaluate best model mechanics
+def create_shap_graph(model, X_tr):
+    """
+    Prints SHAP model
+
+    Parameters:
+    model (Pipeline)
+    X_tr (DataFrame)
+    """
+    preprocessor = model.named_steps['preprocessor']
+
+    model = model.named_steps['model']
+
+    X_tr_transformed = preprocessor.transform(X_tr)
+
+    feature_names = preprocessor.get_feature_names_out()
+
+    explainer = shap.TreeExplainer(model)
+
+    shap_values = explainer.shap_values(X_tr_transformed)
+
+    shap.summary_plot(shap_values, X_tr_transformed, feature_names=feature_names)
+
+# best model without sprint (XGBoost)
+create_shap_graph(xgboost_model_wo, X_tr_wo)
+
+
+# best model with sprint (XGBoost)
+create_shap_graph(xgboost_model_with, X_tr_with)
